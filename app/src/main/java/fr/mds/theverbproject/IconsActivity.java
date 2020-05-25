8d7e5088e442cb0f5ddd1911bc5fa9d9d572b155
Alexis <35.alexis.pele@gmail.com>
2020-05-25 14:51:36 +0200

    - Added Activities for testing purposes

---
+++ app/src/main/java/fr/mds/theverbproject/IconsActivity.java
@@ -0,0 +1,15 @@
+package fr.mds.theverbproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IconsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icons);
+        setTitle("Search - Icons");
+    }
+}
