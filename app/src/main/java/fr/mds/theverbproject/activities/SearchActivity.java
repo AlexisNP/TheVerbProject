3f70849f8522bedfc21c918b893f36617bc3c738
Andrew Conan <andrew.conan1@gmail.com>
2020-05-28 15:20:17 +0200

    insert callApi in SearchActivity to get Icons

--- app/src/main/java/fr/mds/theverbproject/activities/SearchActivity.java
+++ app/src/main/java/fr/mds/theverbproject/activities/SearchActivity.java
@@ -1,14 +1,27 @@
 package fr.mds.theverbproject.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.OnClick;
 import fr.mds.theverbproject.R;
+import fr.mds.theverbproject.adapter.CollectionsAdapter;
+import fr.mds.theverbproject.adapter.IconsAdapter;
+import fr.mds.theverbproject.model.Collection;
+import fr.mds.theverbproject.model.Icon;
+import fr.mds.theverbproject.model.ResponseCollectionResult;
+import fr.mds.theverbproject.model.ResponseIconResult;
+import retrofit2.Call;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -16,6 +29,15 @@ public class SearchActivity extends AppCompatActivity {
     private Button btn_collections;
     private RecyclerView rv_icons;
     private RecyclerView rv_collections;
+
+    private static final int FIRST_PAGE = 1;
+    private static final String TAG = "searchActivity";
+    private Call<ResponseIconResult> callIcons;
+    private Call<ResponseCollectionResult> callCollections;
+    private List<Icon> iconsResults;
+    private IconsAdapter iconsAdapter;
+    private List<Collection> collectionsResults;
+    private CollectionsAdapter collectionsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +74,124 @@ public class SearchActivity extends AppCompatActivity {
                 rv_collections.setLayoutParams(lp_collections);
             }
         });
+    }
+
+
+
+
+
+//    private void loadIcons(final int page) {
+//        GetIconDataService iconDataService = RetrofitInstance.getClient().create(GetIconDataService.class);
+//
+//        callIcons = iconDataService.getIconsByTerm("zombie");
+//
+//        callIcons.enqueue(new Callback<ResponseIconResult>() {
+//            @Override
+//            public void onResponse(@NonNull Call<ResponseIconResult> call, @NonNull Response<ResponseIconResult> response) {
+//
+//                if(page == 1) {
+//                    assert response.body() != null;
+//                    iconsResults = response.body().getIcons();
+//
+//                    iconsAdapter = new IconsAdapter(iconsResults, new IconClickListener() {
+//                        @Override
+//                        public void onIconClick(Icon icon) {
+//                            Intent intent = new Intent(SearchActivity.this, IconActivity.class);
+//                            Bundle bundle = new Bundle();
+//                            bundle.putSerializable("icon", icon);
+//                            intent.putExtras(bundle);
+//                            startActivity(intent);
+//                        }
+//                    });
+//                    rv_icons.setAdapter(iconsAdapter);
+//                } else {
+//                    assert response.body() != null;
+//                    List<Icon> icons = response.body().getIcons();
+//                    for(Icon icon : icons){
+//                        iconsResults.add(icon);
+//                        iconsAdapter.notifyItemInserted(iconsResults.size() - 1);
+//                    }
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(@NonNull Call<ResponseIconResult> call, @NonNull Throwable t) {
+//                Toast.makeText(SearchActivity.this, R.string.something_went_wrong, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+//
+//    private void loadCollections(final int page) {
+//        GetCollectionDataService collectionDataService = RetrofitInstance.getClient().create(GetCollectionDataService.class);
+//
+//        callCollections = collectionDataService.getCollectionBySlug("zombie");
+//
+//        callCollections.enqueue(new Callback<ResponseCollectionResult>() {
+//            @Override
+//            public void onResponse(@NonNull Call<ResponseCollectionResult> call, @NonNull Response<ResponseCollectionResult> response) {
+//
+//                if(page == 1) {
+//                    assert response.body() != null;
+//                    collectionsResults = response.body().getCollection();
+//
+//                    iconsAdapter = new IconsAdapter(collectionsResults, new IconClickListener() {
+//                        @Override
+//                        public void onIconClick(Icon icon) {
+//                            Intent intent = new Intent(SearchActivity.this, IconActivity.class);
+//                            Bundle bundle = new Bundle();
+//                            bundle.putSerializable("icon", icon);
+//                            intent.putExtras(bundle);
+//                            startActivity(intent);
+//                        }
+//                    });
+//                    rv_icons.setAdapter(iconsAdapter);
+//                } else {
+//                    assert response.body() != null;
+//                    List<Icon> icons = response.body().getIcons();
+//                    for(Icon icon : icons){
+//                        iconsResults.add(icon);
+//                        iconsAdapter.notifyItemInserted(iconsResults.size() - 1);
+//                    }
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(@NonNull Call<ResponseCollectionResult> call, @NonNull Throwable t) {
+//                Toast.makeText(SearchActivity.this, R.string.something_went_wrong, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+
+
+
+
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+    public static int getMeasuredPosterHeight(int width) {
+        return (int) (width * 1.5f);
+    }
+
+    public static String iconImagePathBuilder(String imageId) {
+        return "https://static.thenounproject.com/attribution/" +
+                imageId + "-600.png";
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert connectivityManager != null;
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @OnClick(R.id.tv_no_internet_error_refresh)
+    public void refreshActivity(){
+        finish();
+        startActivity(getIntent());
     }
 
 }
