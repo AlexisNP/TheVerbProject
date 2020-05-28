9ac54573671de2d325e79ee99d9f83263f91fc36
Andrew Conan <andrew.conan1@gmail.com>
2020-05-28 12:18:29 +0200

    retrofitInstance is alive now ^^ + test for image on AllIcons Acvitivy

---
+++ app/src/main/java/fr/mds/theverbproject/model/Response.java
@@ -0,0 +1,33 @@
+package fr.mds.theverbproject.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.Set;
+
+public class Response implements Serializable {
+
+    @SerializedName("generated_at")
+    private String generated_at;
+    @SerializedName("icons")
+    private Set<Icon> icons;
+
+    public Response(String generated_at, Set<Icon> icons) {
+        this.generated_at = generated_at;
+        this.icons = icons;
+    }
+
+    public String getGenerated_at() {
+        return generated_at;
+    }
+    public void setGenerated_at(String generated_at) {
+        this.generated_at = generated_at;
+    }
+
+    public Set<Icon> getIcons() {
+        return icons;
+    }
+    public void setIcons(Set<Icon> icons) {
+        this.icons = icons;
+    }
+}
