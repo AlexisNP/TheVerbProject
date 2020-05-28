0574b383664603a30cc6e1830139f1c5a6f969e5
Alexis <35.alexis.pele@gmail.com>
2020-05-28 11:18:59 +0200

    - Added SeekBar events and image scaling

--- app/src/main/java/fr/mds/theverbproject/activities/IconActivity.java
+++ app/src/main/java/fr/mds/theverbproject/activities/IconActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -19,6 +20,7 @@ public class IconActivity extends AppCompatActivity {
     private TextView tv_created_by;
     private TextView tv_collection_name;
     private ImageView iv_icon;
+    private LayoutParams iv_icon_params;
     private SeekBar sb_image_size;
     private Button btn_icon_download;
     private Button btn_icon_favorite;
@@ -36,13 +38,48 @@ public class IconActivity extends AppCompatActivity {
         sb_image_size = findViewById(R.id.sb_image_size);
         btn_icon_download = findViewById(R.id.btn_icon_download);
         btn_icon_favorite = findViewById(R.id.btn_icon_favorite);
+
+        // Sets progress of the SeekBar to half
+        sb_image_size.setProgress(50);
+
+        // Scales icon to a native 400dp * 400dp format
+        scaleIcon(iv_icon, 400, 400);
+
+        sb_image_size.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            // Changes the size of the icon
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Gets the scale factor as a value between 0.0 and 3.0
+                double scale_factor = ((double) progress * 4) / 100;
+                // Scales the icon by that ratio with a minimum height and width or 200dp (half native)
+                scaleIcon(iv_icon, 200 * scale_factor + 200, 200 * scale_factor + 200);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        btn_icon_download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Downloaded");
+            }
+        });
+
+        btn_icon_favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Favourite");
+            }
+        });
     }
 
-    public void onDownload(View v) {
-        Log.d(TAG, "Downloaded");
+    public void scaleIcon(ImageView img, double width, double height) {
+        LayoutParams img_params = img.getLayoutParams();
+        img_params.width = (int) width;
+        img_params.height = (int) height;
+        img.setLayoutParams(img_params);
     }
 
-    public void onFavourite(View v) {
-        Log.d(TAG, "Favourite");
-    }
 }
